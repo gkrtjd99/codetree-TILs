643cfa9f5981f6563e826fd73d6b3b4c97f98724
gkrtjd99 <140094504+gkrtjd99@users.noreply.github.com>
2024-09-06 16:32:01 +0900

    Title: 1부터 특정 수까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240906/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+
+    int sum = 0;
+    for(int i = 0; i <= a; i++)
+    {
+        sum += i;
+    }
+    printf("%d", sum / 10);
+    return 0;
+}
