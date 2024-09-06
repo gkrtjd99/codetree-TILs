70752e581a1b3f067537aad15646733bbb695c8b
gkrtjd99 <140094504+gkrtjd99@users.noreply.github.com>
2024-09-06 22:51:48 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240906/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a; 
+    scanf("%d", &a);
+   
+    if (a % 4 == 0)
+    {
+        if (a % 100 == 0 && a % 400 != 0)
+            printf("false");
+        else
+            printf("true");
+    }
+    else
+        printf("false");
+    return 0;
+}
