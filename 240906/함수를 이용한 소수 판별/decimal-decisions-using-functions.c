cb5e6781761fe107ed2294f051f8176f710348e5
gkrtjd99 <140094504+gkrtjd99@users.noreply.github.com>
2024-09-06 18:26:10 +0900

    Title: 함수를 이용한 소수 판별, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240906/함수를 이용한 소수 판별/decimal-decisions-using-functions.c
+++ 240906/함수를 이용한 소수 판별/decimal-decisions-using-functions.c
@@ -5,6 +5,8 @@ int main() {
     scanf("%d %d", &a, &b);
     for(int i = a; i <= b; i++)
     {
+        if (i == 2)
+            c = 2;
         for (int j = 2; j < i; j++)
         {
             if (i % j == 0)
