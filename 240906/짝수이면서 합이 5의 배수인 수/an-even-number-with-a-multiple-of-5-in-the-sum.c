86dd5ba5770ae9b7826a1b503eb05356ce4ca442
gkrtjd99 <140094504+gkrtjd99@users.noreply.github.com>
2024-09-06 17:15:39 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240906/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.c
+++ 240906/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.c
@@ -4,7 +4,7 @@ int main() {
     int a, c= 0;
     scanf("%d", &a);
     c = (a % 10) + (a / 10);
-    if (n % 2 == 0 && c % 5 == 0)
+    if (a % 2 == 0 && c % 5 == 0)
         printf("Yes");
     else
         printf("No");
