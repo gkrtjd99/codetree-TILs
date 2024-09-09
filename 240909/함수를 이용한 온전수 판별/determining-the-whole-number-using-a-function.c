a7ab682409e18bbf5fee1222677398c20bc1da44
gkrtjd99 <140094504+gkrtjd99@users.noreply.github.com>
2024-09-09 09:43:42 +0900

    Title: 함수를 이용한 온전수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240909/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.c
+++ 240909/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.c
@@ -3,10 +3,13 @@
 int main() {
     int a, b, count = 0;
     scanf("%d %d", &a, &b);
+
     for (int i = a; i <= b; i++)
     {
-        if(1 & i && i % 10 != 5 && (i % 3 != 0 || i % 9 == 0))
+        if (1 & i && i % 10 != 5 && (i % 3 != 0 || i % 9 == 0)) 
+        {
             count++;
+        }
     }
     printf("%d", count);
     return 0;
