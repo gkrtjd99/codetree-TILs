cb16ef1affa1e7abe8591388182730317aaca7dc
gkrtjd99 <140094504+gkrtjd99@users.noreply.github.com>
2024-09-10 10:23:11 +0900

    Title: 두 정수에 대한 연산값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240910/두 정수에 대한 연산값/operational-values-for-two-integers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sw(int *a, int *b)
+{
+    if(a > b)
+    {
+        *a += 25;
+        *b *= 2;
+    }
+    else
+    {
+        *a *= 2;
+        *b += 25;
+    }
+}
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    sw(&a,&b);
+    printf("%d %d",a,b);
+    return 0;
+}
