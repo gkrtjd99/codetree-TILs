b70ebe327f89cce1e6befddf3e475c1f3bd6e525
gkrtjd99 <140094504+gkrtjd99@users.noreply.github.com>
2024-09-18 14:22:23 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240918/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.c
+++ 240918/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int max(int n, int *m, int p)
 {
     if (n == 0)
         return p;
-    else if(m[n] < p)
-        return p;
-    else if (m[n] > p)
-        p = m[n];
-    return (max(n-1,m,p),p);
+    if (m[n - 1] > p)
+        p = m[n - 1];
+    return (max(n-1, m, p));
+
 }
 
 int main() {
     int n, p = 0;
-    int m[n];
+    int *m = (int *)malloc(sizeof(int) * n);
     scanf("%d", &n);
     for(int i = 0; i < n; i++)
         scanf("%d", &m[i]);
     printf("%d", max(n, m, p));
-   
+    free(m);
     return 0;
 }
